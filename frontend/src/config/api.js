149a808a0d14fd76e15858be79c1dd152fd10f8e
Arpit Tiwari <arpittiwari6912@gmail.com>
2025-09-22 12:09:57 +0530

    latest commit

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
+
 const API_BASE_URL = 'https://grafferids-project.onrender.com/';
-=======
-const API_BASE_URL = 'http://localhost:5000';
->>>>>>> b010888b140e6804543176a1d271122dddaee0b3
 
 export default API_BASE_URL;
 
