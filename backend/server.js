ec94b37f8a36ffad2da596dcb3d7bfb7c00031f1
Arpit Tiwari <arpittiwari6912@gmail.com>
2025-09-22 11:59:18 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import companyRoute from "./routes/companyRoute.js";
+import dotenv from 'dotenv'
+import connectDB from "./config/db.js";
+import cors from 'cors';
+
+dotenv.config()
+
+const server = express()
+server.use(cors())
+server.use(express.json())
+
+
+// server.use('/',(req,res)=>{
+//     res.send('hello grafferId')
+// })
+
+server.use('/company', companyRoute)
+// server.use('/review', reviewRoute)
+
+const PORT = 5000;
+
+connectDB().then(()=>{
+    server.listen(PORT, () => {
+        console.log('server started on port', PORT)
+    })
+}).catch((err)=>{console.log(err)})
---
+++ frontend/src/config/api.js
@@ -0,0 +1,3 @@
+const API_BASE_URL = 'https://grafferids-project.onrender.com/';
+
+export default API_BASE_URL;
